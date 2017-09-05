ccb8dfb62e208a9977f2ec6d9a87aedbba73001a
Harika321 <31277769+Harika321@users.noreply.github.com>
2017-09-05 21:37:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,10 +5,16 @@ var element = document.getElementById("main-text");
 
 element.innerHTML = 'new text';
 
+
 // Move the image
-var img = document.getElementById("madi");
+var img = document.getElementById('madi');
+var marginleft = 0;
+function moveright () {
+    marginleft = marginleft + 5;
+    img.style.marginleft = marginleft +'px';
+}
 img.onclick = function () {
-    img.style.marginleft = '100px';
+    var interval = setinterval(moveright,100);
 };
 
 
