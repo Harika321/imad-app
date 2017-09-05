215165c2652c55b6539939974288df741edb5c08
Harika321 <31277769+Harika321@users.noreply.github.com>
2017-09-06 00:05:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,6 +92,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+            counter = counter + 1;
+            res.send(counter.toString());
+});
+}
+
+
      app.get('/:reportName',function(req, res) {
     // reportName = report-one
     // reports[reportName]={} content object for report-one  
