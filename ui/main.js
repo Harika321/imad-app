dde23a3fad01eb9412196821592e1961fc85ebba
Harika321 <31277769+Harika321@users.noreply.github.com>
2017-09-06 00:36:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,21 @@ img.onclick = function () {
 };
 
 
+// counter code
+var button = document.getElementById('counter');
+var counter = 0;
+
+
+button.onclick = function () {
+    
+    //make a request to the counter endpoint
+    
+    //capture the response and store it in a variable
+    
+    //Render the variable in the correct span
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+};
+
+
