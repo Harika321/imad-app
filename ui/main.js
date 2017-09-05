33ace166a91fb4026ae4b5f11799fde15717f2cd
Harika321 <31277769+Harika321@users.noreply.github.com>
2017-09-05 20:57:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,12 @@
 console.log('Loaded!');
 
-//
+//change the tect of the main-text div
+var element = document.getElementById("main-text");
+
+element.innerHTML = 'new text';
 
 
 
-// Move the image
-var img = document.getElementById('madi');
-var marginleft = 0;
-function moveright () {
-    marginleft = marginleft + 5;
-    img.style.marginleft = marginleft +'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveright,100);
-};
+
+
 
