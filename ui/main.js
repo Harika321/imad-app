f5b25c1a37fac45ce426e4acbeca8563121c3660
Harika321 <31277769+Harika321@users.noreply.github.com>
2017-09-05 22:35:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ function moveright () {
     if(marginleft != 200)
     {
     marginleft = marginleft + 5;
-    img.style.marginLeft = marginleft +'px';
+    img.style.marginRight = marginleft +'px';
     }
 }
 img.onclick = function () {
