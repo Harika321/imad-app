42b32be3e4aa19da51f5f6558aac1ea9ca68ad96
Harika321 <31277769+Harika321@users.noreply.github.com>
2017-09-07 11:49:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,8 +32,26 @@ var submit = document.getElementById("submit_btn");
 submit.onclick = function () {
     // Make a request to the server and send the name
     
-    // Capture a 
-}
+    // Capture a list of names and render it as a list
+    var names = ["name1", "name2", "name3", "name4"];
+    var list = "";
+    for (var i=0; i<name length, i++) {
+        list +="<li>" + names[i] + "</li>";
+    }
+    var ul = document.getElenentById("namelist");
+    ul.innerHTML = list;
+};
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
 
 
 
