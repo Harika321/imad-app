82d565d94d3111ccd192c049b2624dc0e194fa60
Harika321 <31277769+Harika321@users.noreply.github.com>
2017-09-07 20:06:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -60,38 +60,4 @@ submit.onclick = function () {
 
 
 
-// Submit name
-var submit = document.getElementById('submit1_btn');
-submit1.onclick = function () {
-    
-    // Create a request
-      var request = new XMLHttpRequest();
-    
-    // Capture the response and store it in a variable
-    request.onreadystatechange = function () {  
-        if (request.readyState === XMLHttpRequest.DONE) {
-            // Take some action
-            if (request.status === 200) {
-                // Capture a list of names and  render it as a list
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list = '';
-                for (var i=0; i< names.length; i++) {
-                    list += '<li>' + names[i] + '</li>';
-                }
-                var ul = document.getElementById('namelist');
-       ul.innerHTML = list;
-            }
-        }
-        // Not done yet
-    };
-  
-    // Make a request
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open("GET", "http://harikachatala09.imad.hasura-app.io/report-one/submit-name?name=" + name, true);
-    request.send(null);
-        
-};
-
 
