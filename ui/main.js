c3d5a2e36971e0fd1444e4853ed5d7f46fce65f0
Harika321 <31277769+Harika321@users.noreply.github.com>
2017-09-08 12:37:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -58,19 +58,19 @@ submit.onclick = function () {
         
 };
 
-// Reset name
-var enterInput = document.getElementById("enter");
-var enter = enterInput.value;
-var reset = document.getElementById("reset_btn");
-reset.onclick = function () {
+// submit name
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onclick = function () {
     //make a request to the server and send the name
     
     // Capture a list of names and render it as a list
-    var enters = ['harika', 'sudheer', 'sai', 'druv'];
+    var names = ['harika', 'abc', 'sai', 'xyz'];
     var list = '';
-  for (var i=0; i< enters.length; i++) {
-     list += '<li>' + enters[i] + '</li>';
+  for (var i=0; i< name.length; i++) {
+     list += '<li>' + names[i] + '</li>';
 }
-var ul = document.getElementById('enterlist');
+var ul = document.getElementById('namelist');
 ul.innerHTML= list;
 };
