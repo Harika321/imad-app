a3762fe4bff9dd9766ec237cdb7ba164f0e1272f
Harika321 <31277769+Harika321@users.noreply.github.com>
2017-09-15 15:25:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ function hash (input, salt) {
 }
 
 app.get('/hash/:input', function(req, res) {
-    var hashedString = hash(req.pramas.input, 'this-is-some-random-string');
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
     res.send(hashedString);
 });
 
